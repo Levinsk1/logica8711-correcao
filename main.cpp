13c55990bb5264202a0e605071de5b2b08c44005
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-03-27 21:57:59 -0300

    teste dados2

--- main.cpp
+++ main.cpp
@@ -4,11 +4,22 @@
 int main(){
 
     std::string nome;
+    int idade;
+    float altura;
 
     std::cout<<"Qual seu nome: "<<std::endl;
     std::cin>>nome;
 
+    std::cout<<"Qual a sua idade: "<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"Qual a sua altura: "<<std::endl;
+    std::cin>>altura;
+
     std::cout<<"Bem vindo(a) "<<nome<<"!!"<<std::endl;
+    std::cout<<"Sua idade é "<<idade<<"!!"<<std::endl;
+    std::cout<<"Sua altura é "<<altura<<"!!"<<std::endl;
+
 
     return 0;
 }
