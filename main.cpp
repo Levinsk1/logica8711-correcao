fb04e509abc8348c44705557d7a0fa38d3a86d9f
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-03-27 21:15:19 -0300

    teste dados pessoais

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
-#include<iomanip>
+#include<string>
 
 int main(){
 
     int idade = 19;
     float altura = 1.72;
     std::string nome = "Arthur";
-    
+
     std::cout<<"meu nome é "<<nome<<"."<<std::endl;
     std::cout<<"minha altura é "<<altura<<"m."<<std::endl;
     std::cout<<"Minha idade é "<<idade<<"anos."<<std::endl;
