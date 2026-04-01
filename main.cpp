086896e21469e38f0295aace6367e46124a99ff9
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-01 19:45:41 -0300

    teste variavel

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 
 int main(){
 
-    char letra = 'E';
+    auto num1 = 10;
+    auto num2 = 15.90f;
+    auto letra = "E";
+    auto texto = "Arthur";
+
+    std::cout<<num1<<std::endl;
+    std::cout<<num2<<std::endl;
     std::cout<<letra<<std::endl;
+    std::cout<<texto<<std::endl;
     
 
 
