38050f1fde4a11544d09a57b2548b6710cc8177a
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-01 19:23:37 -0300

    teste bool

--- main.cpp
+++ main.cpp
@@ -3,23 +3,13 @@
 
 int main(){
 
-    std::string nome;
-    int idade;
-    float altura;
+    bool condicao = true;
+    bool condicao1 = false;
 
-    std::cout<<"Qual seu nome: "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual a sua idade: "<<std::endl;
-    std::cin>>idade;
-
-    std::cout<<"Qual a sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Bem vindo(a) "<<nome<<"!!"<<std::endl;
-    std::cout<<"Sua idade é "<<idade<<"!!"<<std::endl;
-    std::cout<<"Sua altura é "<<altura<<"!!"<<std::endl;
+    std::cout<<condicao<<std::endl;
+    std::cout<<condicao1<<std::endl;
+    
 
 
     return 0;
-}
+} 
