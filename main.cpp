20297a0b12b63319947dc4aafe8d61d1b4b5b2dc
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-01 20:32:01 -0300

    atividade1

--- main.cpp
+++ main.cpp
@@ -3,16 +3,19 @@
 
 int main(){
 
-    auto num1 = 10;
-    auto num2 = 15.90f;
-    auto letra = "E";
-    auto texto = "Arthur";
 
-    std::cout<<num1<<std::endl;
-    std::cout<<num2<<std::endl;
-    std::cout<<letra<<std::endl;
-    std::cout<<texto<<std::endl;
-    
+    std::string nome;
+    int idade;
+    int anoAtual = 2026;
+
+    std::cout<<"Olá qual o seu primeiro nome? "<<std::endl;
+    std::cin>>nome;
+
+    std::cout<<"Qual a sua idade? "<<std::endl; 
+    std::cin>>idade;
+    int anoNascido = anoAtual - idade;
+
+    std::cout<< "Olá" <<nome<< "!! Você nasceu no ano de" << anoNascido <<std::endl;
 
 
     return 0;
