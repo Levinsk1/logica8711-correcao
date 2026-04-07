2aa10e4dfd13a834aef555182c8dc3d7e30ef150
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-06 21:19:33 -0300

    teste contagem caracteres

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 #include<string>
+#include<iostream>
+#include<windows.h> //
 
 int main(){
 
-    int a = 40;
-    int b = 10;
+    std::string nome = "Arthur";
 
-    std::string c = "40";
-    std::string d = "10";
+    std::cout<<"O seu nome tem: "<<nome.length()<<" letras."<<std::endl;
+    std::cout<<nome.size()<<" letras."<<std::endl;
 
-    std::cout<<a+b<<std::endl;
-    std::cout<<c+d<<std::endl;
 
+    SetConsoleOutputCP(CP_UTF8);//
+    SetConsoleCP(CP_UTF8);//
 
     return 0;
 } 
