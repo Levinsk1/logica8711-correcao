7baaa58aa51a651b25fa0b2cda2bdf8436df1f27
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-13 20:27:15 -0300

    teste valor compra

--- main.cpp
+++ main.cpp
@@ -9,22 +9,33 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);//
     SetConsoleCP(CP_UTF8);//
 
-    int idade;
+    int opcaoPagamento;
+    int qtdParafuso;
+    float valorParafuso = 2.00;
+    float valorTotalParafuso;
+    float desconto;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+    std::cout<<"Digite a quantidade de parafusos que deseja: "<<std::endl;
+    std::cin>>qtdParafuso;
 
-    if(idade < 0){
-        std::cout<<"Idade Invalida!"<<std::endl;
-    }else if(idade <= 2){
-        std::cout<<"Você é um bebe?"<<std::endl;
-    }else if(idade <= 8){
-        std::cout<<"Você é uma criança?"<<std::endl;
-    }else if(idade <= 16){
-        std::cout<<"Você é um adolecente?"<<std::endl;
+    valorTotalParafuso = qtdParafuso * valorParafuso;
+
+    std::cout<<"Qual a forma de pagamento: "<<std::endl;
+    std::cout<<"--- 1 - Pix"<<std::endl;
+    std::cout<<"--- 2 - Dinheiro"<<std::endl;
+    std::cout<<"--- 3 - Cartão"<<std::endl;
+    std::cin>>opcaoPagamento;
+
+    if(opcaoPagamento == 1){
+        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }else if(opcaoPagamento == 2){
+        desconto = valorTotalParafuso * 0.90;
+        std::cout<<"Sua compra ficou no valor de R$: "<<desconto<<std::endl;
     }else{
-        std::cout<<"Você é um sênior?"<<std::endl;
+        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
     }
+
+    
     
     
 
