204e843e21a2451d2a596746bb1be43d19560629
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-15 19:15:26 -0300

    loop

--- main.cpp
+++ main.cpp
@@ -9,32 +9,10 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);//
     SetConsoleCP(CP_UTF8);//
 
-    std::string nome;
-    int idade;
-    float altura;
-
-    std::cout<<"Qual seu nome? "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual a sua altura? "<<std::endl;
-    std::cin>>altura;
-
-     std::cout<<"Qual a sua idade? "<<std::endl;
-    std::cin>>idade;
-
-
-    std::cout<<"Bem vindo(a) "<<nome<<"!! Sua altura é "<<altura<<" Você é da ";
-
-
-     if(idade < 0){
-        std::cout<<"Idade Invalida!"<<std::endl;
-    }else if(idade <= 10){
-        std::cout<<"Classe infantil"<<std::endl;
-    }else if(idade <= 18){
-        std::cout<<"Classe adolescente"<<std::endl;
-    }else if(idade <= 60){
-        std::cout<<"Classe adulto"<<std::endl;
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"Isso é um loop!!"<<std::endl;
     }
+    
 
     
     
