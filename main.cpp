b05020b81c7e58f93c012f08e8b9d9a3ccd1d57e
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-15 19:47:42 -0300

    contagem regressiva velocidade 2

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(){
 
     for(int i = 10; i >= 0; i--){
         std::cout<<"A bomba irá explodir em "<<i<<std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::this_thread::sleep_for(std::chrono::seconds(4));
     }
     
         
