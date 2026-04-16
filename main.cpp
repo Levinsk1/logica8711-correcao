ee0a8c9a542383b558956804bdd76a4348a7d108
ARTHUR HENRIQUE LEVINSKI DE LIMA <02521269042@senacrs.edu.br>
2026-04-15 21:06:26 -0300

    soma = soma + i

--- main.cpp
+++ main.cpp
@@ -1,35 +1,26 @@
-#include<iostream>
-#include<string>
-#include<iostream>
-#include<windows.h> //
-#include<cmath>
-#include<thread> // para lidar com o tempo de execução
-#include<chrono> // para definir unidades de tempo (ms, segundos)
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <windows.h> //
+#include <cmath>
+#include <thread> // para lidar com o tempo de execução
+#include <chrono> // para definir unidades de tempo (ms, segundos)
 
-int main(){
+int main()
+{
 
-    SetConsoleOutputCP(CP_UTF8);//
-    SetConsoleCP(CP_UTF8);//
+    SetConsoleOutputCP(CP_UTF8); //
+    SetConsoleCP(CP_UTF8);       //
 
-    int opcao;
 
-    std::cout<<"Digite um valor para opção: "<<std::endl;
-    std::cin>>opcao;
+    int soma = 0;
 
-    switch(opcao){
-    case 1:
-         std::cout<<"Domingo! "<<std::endl;
-    break;
-    case 2:
-        std::cout<<"Segunda-Feira!"<<std::endl;
-    break;
-    default:
-        std::cout<<"Número inválido"<<std::endl;
-    }
-    
-      
+    for(int i = 1; i <= 5; i++){
+        soma = soma + i;
+}
+    std::cout<<"Soma é: "<<soma<<std::endl;
 
 
 
     return 0;
-} 
+}
